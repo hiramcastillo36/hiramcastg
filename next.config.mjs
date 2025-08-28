75a4fba703a0e6e79bbb150453885a0b195cf834
Hiram Castillo <hiramcastillo36@gmail.com>
2025-08-28 17:21:43 -0600

    Add gh-pages deployment script and update dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,6 +10,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: isProd ? 'export' : 'standalone',
+  assetPrefix: isProd ? '/hiramcastg/' : '',
+  basePath: isProd ? '/hiramcastg' : '',
 }
 
 export default nextConfig
